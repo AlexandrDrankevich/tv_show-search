c8ad2de4130d869f50d40867b436be52e7fb54ca
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-06-04 16:42:12 +0300

    refactor

--- src/test/java/com/tvmaze/ui/utils/UserCreator.java
+++ src/test/java/com/tvmaze/ui/utils/UserCreator.java
@@ -7,7 +7,6 @@ public class UserCreator {
     public static final String USER_PASSWORD = "27052009";
 
     public static User createDefaultUser() {
-
         return new User(USER_NAME, USER_PASSWORD);
     }
 
